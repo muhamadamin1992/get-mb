dbb929799bbccde0deb5d884392c94ab3305c5ee
Muhamadamin <muhamadamin1992@gmail.com>
2020-08-17 14:03:23 +0500

    feat: complete version 1.0

--- App.tsx
+++ App.tsx
@@ -1,21 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, Header, Content, List, Body, Title } from 'native-base';
+import Item from './components/Item';
 
-export default function App() {
+const items = [
+  {
+    title: "500 MB",
+    price: "10 000 so'm",
+    url: "tel:*147*10072*39722%23"
+  },
+  {
+    title: "1500 MB",
+    price: "15 000 so'm",
+    url: "tel:*147*10073*39722%23"
+  },
+  {
+    title: "3000 MB",
+    price: "24 000 so'm",
+    url: "tel:*147*10074*39722%23"
+  },
+  {
+    title: "5000 MB",
+    price: "32 000 so'm",
+    url: "tel:*147*10075*39722%23"
+  },
+  {
+    title: "8000 MB",
+    price: "41 000 so'm",
+    url: "tel:*147*10076*39722%23"
+  },
+  {
+    title: "12 000 MB",
+    price: "50 000 so'm",
+    url: "tel:*147*10077*39722%23"
+  },
+  {
+    title: "20 000 MB",
+    price: "65 000 so'm",
+    url: "tel:*147*10078*39722%23"
+  },
+  {
+    title: "30 000 MB",
+    price: "75 000 so'm",
+    url: "tel:*147*10079*39722%23"
+  },
+  {
+    title: "50 000 MB",
+    price: "85 000 so'm",
+    url: "tel:*147*10080*39722%23"
+  },
+  {
+    title: "75 000 MB",
+    price: "110 000 so'm",
+    url: "tel:*147*10150*39722%23"
+  },
+]
+
+export default function App (props) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Header>
+        <Body style={{justifyContent: "center"}}>
+          <Title>Megabayt</Title>
+        </Body>
+      </Header>
+      <Content>
+        <List>
+          {items.map((item) => {
+            return <Item {...item} />
+          })}
+        </List>
+      </Content>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Item.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Linking } from "react-native";
+import { Text, ListItem, Button, Body } from 'native-base';
+
+export default function Item (props) {
+  function handePress() {
+    Linking.openURL(props.url);
+  }
+  return (
+    <ListItem >
+      <Body>
+        <Button block onPress={handePress}><Text>{ props.title }: { props.price }</Text></Button>
+      </Body>
+    </ListItem>
+  );
+}
