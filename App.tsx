6c15b8c925deb5cc802dc206a48478e89aa699d9
Muhamadamin <muhamadamin1992@gmail.com>
2020-10-05 21:45:55 +0500

    refactor: move repeated url parts to constants

--- App.tsx
+++ App.tsx
@@ -3,56 +3,59 @@ import { Container, Header, Content, List, Body, Title } from 'native-base';
 import Item from './components/Item';
 import IItem from "./models/item";
 
+const urlStart:string = 'tel:*147*';
+const urlEnd:string = '*39722%23';
+
 const items: Array<IItem> = [
   {
     title: "500 MB",
     price: "10 000 so'm",
-    url: "tel:*147*10072*39722%23"
+    url: `${urlStart}10072${urlEnd}`,
   },
   {
     title: "1500 MB",
     price: "15 000 so'm",
-    url: "tel:*147*10073*39722%23"
+    url: `${urlStart}10073${urlEnd}`,
   },
   {
     title: "3000 MB",
     price: "24 000 so'm",
-    url: "tel:*147*10074*39722%23"
+    url: `${urlStart}10074${urlEnd}`,
   },
   {
     title: "5000 MB",
     price: "32 000 so'm",
-    url: "tel:*147*10075*39722%23"
+    url: `${urlStart}10075${urlEnd}`,
   },
   {
     title: "8000 MB",
     price: "41 000 so'm",
-    url: "tel:*147*10076*39722%23"
+    url: `${urlStart}10076${urlEnd}`,
   },
   {
     title: "12 000 MB",
     price: "50 000 so'm",
-    url: "tel:*147*10077*39722%23"
+    url: `${urlStart}10077${urlEnd}`,
   },
   {
     title: "20 000 MB",
     price: "65 000 so'm",
-    url: "tel:*147*10078*39722%23"
+    url: `${urlStart}10078${urlEnd}`,
   },
   {
     title: "30 000 MB",
     price: "75 000 so'm",
-    url: "tel:*147*10079*39722%23"
+    url: `${urlStart}10079${urlEnd}`,
   },
   {
     title: "50 000 MB",
     price: "85 000 so'm",
-    url: "tel:*147*10080*39722%23"
+    url: `${urlStart}10080${urlEnd}`,
   },
   {
     title: "75 000 MB",
     price: "110 000 so'm",
-    url: "tel:*147*10150*39722%23"
+    url: `${urlStart}10150${urlEnd}`,
   },
 ]
 
